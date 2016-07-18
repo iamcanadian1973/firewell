b59066207272f93dcdbadf4f5acb0642696ef06b
Kyle Rumble <kylerumble@gmail.com>
2016-07-18 10:11:22 -0700

    Search form changes

--- wp-content/themes/firewell/searchform.php
+++ wp-content/themes/firewell/searchform.php
@@ -6,14 +6,10 @@
  * @since _s 1.0
  */
  
- static $search_form_id;
- $search_form_id++;
 ?>
-	<div id="search-form-<?php echo $search_form_id;?>" class="searchbox">
-	<form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-		<label for="s" class="screen-reader-text"><?php _ex( 'Search', 'assistive text', '_s' ); ?></label>
+	<form method="get" id="searchform" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+		<label for="s">
 		<input type="search" class="searchbox-input" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search&hellip;', 'placeholder', '_s' ); ?>" />
-		<input type="submit" class="searchbox-submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Go', 'submit button', '_s' ); ?>" />
-		<span class="searchbox-icon">Go</span>
+		</label>
+		<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', '_s' ); ?>" />
 	</form>
-	</div>
