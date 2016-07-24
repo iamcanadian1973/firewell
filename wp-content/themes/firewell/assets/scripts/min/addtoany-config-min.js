2d001f4e0337031cee41d4514b8e0d92013e9895
Kyle Rumble <kylerumble@gmail.com>
2016-07-24 10:42:11 -0700

    removed uneeded javascript

--- wp-content/themes/firewell/assets/scripts/min/addtoany-config-min.js
+++
@@ -1,1 +0,0 @@
-!function($){"use strict";a2a_config.templates={email:{subject:"Check this out: ${title}",body:"Click the link:\n${link}"},twitter:"Reading: ${title} ${link} by @firewell"}}(jQuery);
