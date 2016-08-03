06b575b6aa9d2ea918e05df5fc435b61732f3fc4
Kyle Rumble <kylerumble@gmail.com>
2016-08-03 10:05:46 -0700

    Redirect non-admins to the homepage after logging into the site.

--- wp-content/themes/firewell/lib/functions/members.php
+++ wp-content/themes/firewell/lib/functions/members.php
@@ -1,21 +1,41 @@
 <?php
 // Firewell Member functions
 
-/*
- show the membersonly message
- get_template_part( 'template-parts/content-members-only', 'none' );
-*/
+/**
+ * Redirect non-admins to the homepage after logging into the site.
+ *
+ * @since 	1.0
+ */
+function firewell_login_redirect( $redirect_to, $request, $user  ) {
+	return ( is_array( $user->roles ) && in_array( 'administrator', $user->roles ) ) ? admin_url() : site_url();
+}
 
+add_filter( 'login_redirect', 'firewell_login_redirect', 10, 3 );
+
+/**
+ * Get current user role
+ *
+ * @since 	1.0
+ */
 function firewell_get_user_role( $user = null ) {
 	$user = $user ? new WP_User( $user ) : wp_get_current_user();
 	return $user->roles ? array_values( $user->roles )[0] : false;
 }
 
+/**
+ * Find out if they are a user. We do it this way so that anyone that can read can see content.
+ *
+ * @since 	1.0
+ */
 function firewell_is_member() {
 	return current_user_can( 'read' ) ? true : false;
 }
 
-
+/**
+ * Format he Members message
+ *
+ * @since 	1.0
+ */
 function firewell_members_only_message() {
 	
 	$message = get_field( 'members_only_message', 'options' );
@@ -27,6 +47,8 @@ function firewell_members_only_message() {
 	return false;
 }
 
+/*
+// Can't remember what this was supposed to do, but keeping it anyway for now.
 add_action( 'firewell_after_content', 'check_content_permission' );
 function check_content_permission() {
 	global $post;
@@ -37,13 +59,16 @@ function check_content_permission() {
 	}
 	
 }
+*/
 
 
 
-
-
-// Set page builder error message for users that are not logged in
-add_action( 'page_builder_after_while', function() {
+/**
+ * Set page builder error message for users that are not logged in
+ *
+ * @since 	1.0
+ */
+ add_action( 'page_builder_after_while', function() {
 		
 	// Is the page restricted? If so only show content blocks that are set to visible
 	if( !members_can_current_user_view_post( get_the_ID() ) ) {
